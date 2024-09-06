d9cd2877d1f0ec66ee31defa9fbeb0af362ff92d
ChokJianHong <116249603+ChokJianHong@users.noreply.github.com>
2024-09-06 12:19:04 +0800

    Schedule
    I think it is complete while history is still not complete

---
+++ technician_app/lib/Pages/History.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:technician_app/Assets/Components/AppBar.dart';
+import 'package:technician_app/Assets/Components/BottomNav.dart';
+
+class History extends StatefulWidget {
+  const History({super.key});
+
+  @override
+  State<History> createState() => _HistoryState();
+}
+
+class _HistoryState extends State<History> {
+
+  int _currentIndex = 1;
+
+  void _onTapTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF391370),
+      appBar: CustomAppBar(),
+      bottomNavigationBar: BottomNav(
+        onTap: _onTapTapped,
+        currentIndex: _currentIndex,
+      ),
+    );
+  }
+}
