177d21ec27936959d7c544f6b2f92253322bdf38
ChokJianHong <116249603+ChokJianHong@users.noreply.github.com>
2024-09-15 14:03:45 +0800

    Update History.dart

--- technician_app/lib/Pages/History.dart
+++ technician_app/lib/Pages/History.dart
@@ -31,7 +31,7 @@ class _HistoryState extends State<History> {
             child: SearchBar(
               hintText: "Search",
               controller: controller,
-              padding: const WidgetStatePropertyAll<EdgeInsets>(
+              padding: const MaterialStatePropertyAll<EdgeInsets>(
                 EdgeInsets.symmetric(horizontal: 16.0),
               ),
               onTap: () {
