95bf8129c8d73dd9d9b1ae3b883715701e1a8c70
ChokJianHong <116249603+ChokJianHong@users.noreply.github.com>
2024-10-27 18:22:31 +0800

    update

---
+++ technician_app/lib/Assets/Components/request_card.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class RequestCard extends StatefulWidget {
+  final String token;
+  const RequestCard({super.key, required this.token});
+
+  @override
+  State<RequestCard> createState() => _RequestCardState();
+}
+
+class _RequestCardState extends State<RequestCard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
